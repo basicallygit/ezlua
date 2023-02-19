0423e915121324a861af9587f0ef3538425328c1
basicallygit <91993321+basicallygit@users.noreply.github.com>
2023-02-19 00:33:08 +0000

    fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -8,94 +8,94 @@
 class LuaVirtualMachine
 {
 private:
-	lua_State *L;
+    lua_State *L;
 
-	void PrintLastError()
-	{
-		std::cout << "Error in lua virtual machine:\n" << lua_tostring(L, -1) << std::endl;
-	}
+    void PrintLastError()
+    {
+	std::cout << "Error in lua virtual machine:\n" << lua_tostring(L, -1) << std::endl;
+    }
 public:
-	LuaVirtualMachine()
-	{
-		L = luaL_newstate();
-	}
+    LuaVirtualMachine()
+    {
+	L = luaL_newstate();
+    }
 
-	void LoadStdLib()
-	{
-		luaL_openlibs(L);
-	}
+    void LoadStdLib()
+    {
+	luaL_openlibs(L);
+    }
 
-	bool Execute(const char* code)
-	{
-		int r = luaL_dostring(L, code);
-		if (r == LUA_OK) {
-			return true;
-		}
-		else {
-			PrintLastError();
-			return false;
-		}
+    bool Execute(const char* code)
+    {
+	int r = luaL_dostring(L, code);
+	if (r == LUA_OK) {
+	    return true;
 	}
+	else {
+	    PrintLastError();
+	    return false;
+	}
+    }
 
-	float GetNumber(const char* identifier)
+    float GetNumber(const char* identifier)
+    {
+	lua_getglobal(L, identifier);
+	if (lua_isnumber(L, -1))
 	{
-		lua_getglobal(L, identifier);
-		if (lua_isnumber(L, -1))
-		{
-			return (float)lua_tonumber(L, -1);
-		}
-		else
-		{
-			PrintLastError();
-			return -1;
-		}
+	    return (float)lua_tonumber(L, -1);
 	}
+	else
+	{
+	    PrintLastError();
+	    return -1;
+	}
+    }
 
-	bool ExecuteFile(const char* filename)
-	{
-		int r = luaL_dofile(L, filename);
-		if (r == LUA_OK) {
-			return true;
-		}
-		else {
-			PrintLastError();
-			return false;
-		}
+    bool ExecuteFile(const char* filename)
+    {
+	int r = luaL_dofile(L, filename);
+	if (r == LUA_OK) {
+	    return true;
 	}
+	else {
+	    PrintLastError();
+	    return false;
+	}
+    }
 
-	~LuaVirtualMachine()
-	{
-		lua_close(L);
-	}
+    ~LuaVirtualMachine()
+    {
+	lua_close(L);
+    }
 };
 
 #if EXECUTABLE == 1
 
 int main()
 {
-	using namespace std;
+    using namespace std;
 
-	LuaVirtualMachine lvm;
-	string input;
-	while (true) {
-		cout << "> ";
-		getline(cin, input);
-		if (input == ":exit" || input == ":quit") {
-			break;
-		}
-		else if (input == ":loadlibs" || input == ":loadstd") {
-			lvm.LoadStdLib();
-		}
-		else if (input == ":loadfile" || input == ":runfile") {
-			cout << "Filename: ";
-			getline(cin, input);
-			lvm.ExecuteFile(input.c_str());
-		}
-		else {
-			lvm.Execute(input.c_str());
-		}
+    LuaVirtualMachine lvm;
+    string input;
+    while (true) {
+	cout << "> ";
+	getline(cin, input);
+	if (input == ":exit" || input == ":quit") {
+	    break;
 	}
-	return 0;
+	else if (input == ":loadlibs" || input == ":loadstd") {
+	    lvm.LoadStdLib();
+	}
+	else if (input == ":loadfile" || input == ":runfile") {
+	    cout << "Filename: ";
+	    getline(cin, input);
+	    lvm.ExecuteFile(input.c_str());
+	}
+	else {
+	    lvm.Execute(input.c_str());
+	}
+    }
+    return 0;
 }
 
 #endif
